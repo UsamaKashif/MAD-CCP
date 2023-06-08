d9a8d6c2a984031cb18bf4f50065e22a68d3e6a0
usamakashif <usamakashif1@gmail.com>
2023-06-08 12:57:48 +0500

    Sign up screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:mad_ccp/utils/colors.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -10,6 +12,37 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("Sign Up"),
+              const TextField(
+                decoration: InputDecoration(
+                    hintText: "Email Address", label: Text("Email Address")),
+              ),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    hintText: "Password", label: Text("Password")),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.primaryColor,
+                    foregroundColor: Colors.white),
+                onPressed: () {},
+                child: const Text("Sign Up"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
